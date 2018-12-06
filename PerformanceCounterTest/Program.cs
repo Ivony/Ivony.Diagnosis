a258ba8153c69e3d1c0e988008f5843a1020ee20
Ivony <ivony@live.com>
2018-12-06 16:50:13 +0800

    upgrade to dotnet core 2.2

---
+++ PerformanceCounterTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceCounterTest
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+
+      foreach ( var item in PerformanceCounterCategory.GetCategories() )
+      {
+        Console.WriteLine( item.CategoryName );
+      }
+
+    }
+  }
+}
