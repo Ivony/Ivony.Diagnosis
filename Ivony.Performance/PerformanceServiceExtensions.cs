0692cd9dadc0cbfbc9dabcd9e5f8eb31ad3b3048
Ivony <ivony@live.com>
2019-01-21 14:37:30 +0800

    fix IPerformanceCounterFactory implement

--- Ivony.Performance/PerformanceServiceExtensions.cs
+++ Ivony.Performance/PerformanceServiceExtensions.cs
@@ -19,10 +19,37 @@ namespace Ivony.Performance
     /// <returns>返回一个 IDisposable 对象，用于取消注册性能报告搜集</returns>
     public static IDisposable Register<TReport>( this IPerformanceService service, IPerformanceCounter<TReport> counter ) where TReport : IPerformanceReport
     {
-      return service.Register( counter, service.ServiceProvider.GetServices<IPerformanceReportCollector<TReport>>().ToArray() );
+      return service.Register( counter, service.ServiceProvider.GetPerformanceReportCollectors<TReport>( counter ) );
     }
 
 
+
+    /// <summary>
+    /// 从系统服务中查找性能报告搜集器
+    /// </summary>
+    /// <typeparam name="TReport">性能报告类型</typeparam>
+    /// <param name="serviceProvider">系统服务提供程序</param>
+    /// <param name="counter">性能计数器</param>
+    /// <returns>所有注册的性能报告搜集器</returns>
+    public static IPerformanceReportCollector<TReport>[] GetPerformanceReportCollectors<TReport>( this IServiceProvider serviceProvider, IPerformanceCounter<TReport> counter ) where TReport : IPerformanceReport
+    {
+
+      var result = new HashSet<IPerformanceReportCollector<TReport>>();
+      var factories = serviceProvider.GetServices<IPerformanceReportCollectorFactory>();
+
+      foreach ( var item in factories )
+      {
+        var collector = item.GetReportCollector<TReport>( counter );
+        if ( collector != null )
+          result.Add( collector );
+      }
+
+
+      result.UnionWith( serviceProvider.GetServices<IPerformanceReportCollector<TReport>>().ToArray() );
+
+      return result.ToArray();
+
+    }
 
 
 
